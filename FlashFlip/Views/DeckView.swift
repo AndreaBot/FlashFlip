8affc11175bd7103ebc91c958be06875cd1f1b63
Andrea Bottino <bottinoandrea93@gmail.com>
2024-05-25 11:14:24 +0100

    DeckView: added empty deck message

--- FlashFlip/Views/DeckView.swift
+++ FlashFlip/Views/DeckView.swift
@@ -18,8 +18,9 @@ struct DeckView: View {
     var context: ModelContext
     
     var body: some View {
-            GeometryReader { metrics in
-                VStack {
+        GeometryReader { metrics in
+            VStack {
+                if !deck.cards.isEmpty {
                     ScrollView(.horizontal) {
                         HStack {
                             ForEach(deck.cards) { card in
@@ -29,100 +30,102 @@ struct DeckView: View {
                         }
                         .frame(height: metrics.size.height * 0.33)
                     }
-                    
-                    Spacer()
-                    
-                    Form {
-                        Section {
-                            HStack {
-                                Text("Number of sessions:")
-                                Spacer()
-                                Text("\(deck.sessionsCount)")
-                            }
-                            HStack {
-                                Text("Average result:")
-                                Spacer()
-                                Text(deck.sessionsCount == 0 ? "/" : String(format: "%.2f", deck.averageCorrectAnswers)+"%")
-                            }
-                        }
-                        
-                        Section {
-                            NavigationLink("Browse cards") {
-                                CardsBrowser(deck: deck, context: context)
-                            }
-                            .disabled(deck.cards.isEmpty)
-                            
-                            Button {
-                                showCardCreation = true
-                            } label: {
-                                Text("Create new cards")
-                            }
-                            .buttonStyle(.borderless)
-                        }
-                        
-                        Section {
-                            NavigationLink("Start study session") {
-                                StudySessionView(viewModel: StudySessionViewModel(deck: deck, studyCards: deck.cards.shuffled(), practisingWeakCards: false))
-                            }
-                            .disabled(deck.cards.isEmpty)
-                            
-                            NavigationLink("Practice weak cards") {
-                                StudySessionView(viewModel: StudySessionViewModel(deck: deck, studyCards: StudySessionViewModel.createWeakCardsSession(deck: deck), practisingWeakCards: true))
-                            }
-                            .disabled(deck.cards.isEmpty)
-                        }
-                        
-                       
-                    }
+                } else {
+                    Text("Your deck is currently empty!")
+                        .frame(height: metrics.size.height * 0.33)
                 }
-                .navigationTitle(deck.name)
-                .toolbar {
-                    ToolbarItemGroup(placement: .confirmationAction) {
-                        Button {
-                            showDeckNameEditing = true
-                        } label: {
-                            Image(systemName: "pencil")
-                                .fontWeight(.semibold)
+                
+                Spacer()
+                
+                Form {
+                    Section {
+                        HStack {
+                            Text("Number of sessions:")
+                            Spacer()
+                            Text("\(deck.sessionsCount)")
                         }
+                        HStack {
+                            Text("Average result:")
+                            Spacer()
+                            Text(deck.sessionsCount == 0 ? "/" : String(format: "%.2f", deck.averageCorrectAnswers)+"%")
+                        }
+                    }
+                    
+                    Section {
+                        NavigationLink("Browse cards") {
+                            CardsBrowser(deck: deck, context: context)
+                        }
+                        .disabled(deck.cards.isEmpty)
+                        
                         Button {
                             showCardCreation = true
                         } label: {
-                            Image(systemName: "plus")
-                                .fontWeight(.semibold)
+                            Text("Create new cards")
                         }
+                        .buttonStyle(.borderless)
                     }
-                }
-                .sheet(isPresented: $showCardCreation) {
-                    CardCreationView(deck: deck, context: context, cardIsBeingModified: false)
-                        .presentationDetents([.fraction(0.34)])
-                }
-                .alert("Edit Deck name", isPresented: $showDeckNameEditing) {
-                    TextField("Type the new name", text: $newDeckName)
-                        .foregroundStyle(.black)
-                        .onSubmit {
-                            if newDeckName.isEmpty {
-                                return
-                            }
-                            deck.name = newDeckName
-                            showDeckNameEditing = false
+                    
+                    Section {
+                        NavigationLink("Start study session") {
+                            StudySessionView(viewModel: StudySessionViewModel(deck: deck, studyCards: deck.cards.shuffled(), practisingWeakCards: false))
                         }
-                    Button("Cancel") {}
-                    Button("Confirm") {
-                        deck.name = newDeckName
+                        .disabled(deck.cards.isEmpty)
+                        
+                        NavigationLink("Practice weak cards") {
+                            StudySessionView(viewModel: StudySessionViewModel(deck: deck, studyCards: StudySessionViewModel.createWeakCardsSession(deck: deck), practisingWeakCards: true))
+                        }
+                        .disabled(deck.cards.isEmpty)
                     }
-                    .disabled(newDeckName.isEmpty)
-                }
-                .onChange(of: showDeckNameEditing) { _, _ in
-                    newDeckName = deck.name
                 }
             }
+            .navigationTitle(deck.name)
+            .toolbar {
+                ToolbarItemGroup(placement: .confirmationAction) {
+                    Button {
+                        showDeckNameEditing = true
+                    } label: {
+                        Image(systemName: "pencil")
+                            .fontWeight(.semibold)
+                    }
+                    Button {
+                        showCardCreation = true
+                    } label: {
+                        Image(systemName: "plus")
+                            .fontWeight(.semibold)
+                    }
+                }
+            }
+            .sheet(isPresented: $showCardCreation) {
+                CardCreationView(deck: deck, context: context, cardIsBeingModified: false)
+                    .presentationDetents([.fraction(0.34)])
+            }
+            .alert("Edit Deck name", isPresented: $showDeckNameEditing) {
+                TextField("Type the new name", text: $newDeckName)
+                    .foregroundStyle(.black)
+                    .onSubmit {
+                        if newDeckName.isEmpty {
+                            return
+                        }
+                        deck.name = newDeckName
+                        showDeckNameEditing = false
+                    }
+                Button("Cancel") {}
+                Button("Confirm") {
+                    deck.name = newDeckName
+                }
+                .disabled(newDeckName.isEmpty)
+            }
+            .onChange(of: showDeckNameEditing) { _, _ in
+                newDeckName = deck.name
+            }
         }
+    }
 }
 
 
 //#Preview {
 //    let config = ModelConfiguration(isStoredInMemoryOnly: true)
 //    let container = try! ModelContainer(for: FolderModel.self, configurations: config)
-//    
+//
 //    return DeckView(deck: DeckModel(id: UUID(), name: "Test"), context: ModelContext(container))
 //}
